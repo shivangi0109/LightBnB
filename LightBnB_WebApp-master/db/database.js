c0b1fd25b62980a6848e84058866d538e9c2989c
Shivangi Srivastava <shi06dec@gmail.com>
2023-08-08 21:26:03 +0000

    LighthouseBnB Users Queries - getUserWithEmail, getUserWithId & addUser

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -17,7 +17,7 @@ const getAllProperties = (options, limit = 10) => {
   return pool
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
-      console.log(result.rows);
+      // console.log(result.rows);
       // Return the rows from the query result
       return result.rows;
     })
@@ -34,16 +34,21 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    // if (user?.email.toLowerCase() === email?.toLowerCase()) {
-    if (user && user.email && user.email.toLowerCase() === email && email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1`, [email])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null; // User with the given email doesn't exist
+      }
+      // console.log(result.rows[0]);
+      return result.rows[0]; // Return the user object
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
+// getUserWithEmail('michaelgray@mail.com');
 
 /**
  * Get a single user from the database given their id.
@@ -51,8 +56,22 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null; // User with the given id doesn't exist
+      }
+      // console.log(result.rows);
+      // console.log(result.rows[0]);
+      return result.rows[0]; // Return the user object
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
+
+// getUserWithId(7);
 
 /**
  * Add a new user to the database.
@@ -60,10 +79,17 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(
+      `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`, // Add RETURNING *; to the end of an INSERT query to return the objects that were inserted. This is handy when you need the auto generated id of an object you've just added to the database
+      [user.name, user.email, user.password]
+    )
+    .then((result) => {
+      return result.rows[0]; // Return the new user object with the auto-generated id
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Reservations
